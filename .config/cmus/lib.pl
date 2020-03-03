5152672821ed4adb4802d589f43f141477f21b0b
Babar Ahmad <babarlinuxguy@gmail.com>
2020-03-04 02:59:44 +0500

    added bashrc zshrc bash profile and .config directory which have polybar and other configurations

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,6 @@
+/home/tux/Music/Sochenge Tumhe Pyar Kare Ke Nahi ( Deewana (1992)(MP3_128K).mp3
+/home/tux/Music/Mera Dil Bhi Kitna Paagal Hai _ Saajan _ Kumar San(MP3_128K).mp3
+/home/tux/Music/-Dheere-Dheere-Se-Meri-Zindagi-Mein-Aana-(RaagSong.Com).mp3
+/home/tux/Music/Kitna Haseen Chehra _ Dilwale Songs _ Ajay Devgan(MP3_128K).mp3
+/home/tux/Music/Tere Dard Se Dil Aabad Raha (HD) _ Deewana Song _(MP3_128K).mp3
+/home/tux/Music/1942 A Love Story _ Ek Ladki Ko Dekha Toh _ Anil K(MP3_128K).mp3
